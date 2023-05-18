b4798409858ab4662aa79bbd1ff8a2947cfa0917
Oxton Lena <133621940+OxtonLena@users.noreply.github.com>
2023-05-18 09:45:14 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+File: main.py
+Author: Oxton Lena
+Created Time: 2023-05-18 09:42:43
+"""
+
+import socket
+
+
+def main():
+    """main function"""
+    print("Sniffer running...")
+
+
+if __name__ == '__main__':
+    main()
